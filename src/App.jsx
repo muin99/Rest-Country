ddf61bab3b18da5596756d5bc030e3131e0bf4e1
muin99 <itsmuin99@gmail.com>
2024-10-25 16:15:26 +0600

    Added all country

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+  return (
+    <div>
+      <h1>React</h1>
+      <Countries></Countries>
+    </div>
+  )
+}
+
+export default App
