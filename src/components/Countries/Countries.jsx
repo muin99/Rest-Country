e562f28a7fd0f6126f3c6a1b0692fa76a3a5d4e3
muin99 <itsmuin99@gmail.com>
2024-10-26 18:45:29 +0600

    finished

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -38,7 +38,7 @@ const Countries = () => {
             </div>
             <div className="flag-container">
                     {
-                        visitedFlags.map(flag, idx => <img key=  {idx} src={flag} alt="flag"/>)
+                        visitedFlags.map((flag, idx) => <img key= {idx} src={flag} alt="flag"/>)
                     }
             </div>
             <div className="country-container">
